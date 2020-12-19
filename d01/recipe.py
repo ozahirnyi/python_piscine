4598d6489afd606036d6f58eaf0cf5fd2f00d6f1
ozahirnyi <ozahirny@gmail.com>
2020-12-19 15:44:59 +0200

    start to testing Recipe

--- d01/recipe.py
+++ d01/recipe.py
@@ -6,20 +6,39 @@ class Recipe:
         self.ingredients = ingredients
         self.description = description
         self.recipe_type = recipe_type
+        self.__str__()
 
     def push_data(self):
-        if not isinstance(str, self.name):
+        if not isinstance(self.name, str):
             raise AttributeError
-        if not isinstance(int, self.cook_lvl) or self.cook_lvl not in range(0, 5):
+        if not isinstance(self.cook_lvl, int) or self.cook_lvl not in range(0, 5):
             raise AttributeError
-        if not isinstance(int, self.cook_time) or int(self.cook_time) < 0:
+        if not isinstance(self.cook_time, int) or int(self.cook_time) < 0:
             raise AttributeError
-        if not isinstance(list, self.ingredients):
+        if not isinstance(self.ingredients, list):
             raise AttributeError
-        if not isinstance(str, self.description):
+        if not isinstance(self.description, str):
             raise AttributeError
-        if not isinstance(str, self.recipe_type) or self.recipe_type != "starter":
+        if not isinstance(self.recipe_type, str) or (self.recipe_type != "starter"
+                                                     and self.recipe_type != "lunch"
+                                                     and self.recipe_type != "dessert"):
             raise AttributeError
 
     def __str__(self):
-        return self
+        try:
+            self.push_data()
+            res = ""
+            res = res.join("Name: " + self.name + '\n')
+            res = "".join(res + "Cook_lvl: " + str(self.cook_lvl) + '\n')
+            res = "".join(res + "Cook_time: " + str(self.cook_time) + '\n')
+            res = "".join(res + "Ingredients: " + str(self.ingredients) + '\n')
+            res = "".join(res + "Description: " + self.description + '\n')
+            res = "".join(res + "Recipe_type: " + self.recipe_type)
+            return res
+        except AttributeError:
+            print("• name (str)\n"
+                  "• cooking_lvl (int) : range 1 to 5\n"
+                  "• cooking_time (int) : in minutes (no negative numbers)\n"
+                  "• ingredients (list) : list of all ingredients each represented by a string\n"
+                  "• description (str) : description of the recipe\n"
+                  "• recipe_type (str) : can be 'starter', 'lunch' or 'dessert'.)")
--- d01/test.py
+++ d01/test.py
@@ -1,4 +1,4 @@
 from recipe import Recipe
 
-rec = Recipe("grecha", 2, 15.2, ["kz", "25 kartoshek", "12 manda"], "nu eta ochen vkusna, pravda", "sturter")
-print(rec.recipe_type)
+rec = Recipe("grecha", 2, 15, ["kz", "25 kartoshek", "12 manda"], "nu eta ochen vkusna, pravda", "starter")
+print(rec)
