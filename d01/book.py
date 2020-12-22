0c58c827484aea431cb640ca683026131e840170
ozahirnyi <ozahirny@gmail.com>
2020-12-22 15:38:05 +0200

    book class done

--- d01/book.py
+++ d01/book.py
@@ -12,15 +12,22 @@ class Book:
         print("Book created!")
         self.creation_date = datetime.datetime.now()
 
-    # def get_recipes_by_types(self, recipe_type):
+    def get_recipes_by_name(self, name):
+        for key, value in self.recipes_list.items():
+            if key.split(' ', 1)[0] == name:
+                return value
+        return "There is nothing to print)"
 
-    def get_recipe_by_name(self, name):
-        return self.recipes_list.get(name)
+    def get_recipe_by_types(self, recipe_type):
+        for key, value in self.recipes_list.items():
+            if key.split(' ', 1)[1] == recipe_type:
+                print(value.__str__() + '\n')
 
     def add_recipe(self, recipe):
         try:
             Recipe.check_data(recipe)
-            self.recipes_list.update({recipe.recipe_type: recipe})
+            buf = {recipe.get_name_n_type(): recipe}
+            self.recipes_list.update(buf)
             self.last_update = datetime.datetime.now()
         except AttributeError:
             print("Ne naebesh, ne prozhivesh?)")
--- d01/test.py
+++ d01/test.py
@@ -1,13 +1,13 @@
 from recipe import Recipe
 from book import Book
 
-rec = Recipe("grecha", 1, 15, ["kz", "25 kartoshek", "12 manda"],
+rec = Recipe("grecha1", 1, 15, ["kz", "25 kartoshek", "12 manda"],
              "nu eta ochen vkusna, pravda", "starter")
-rec1 = Recipe("grecha", 2, 15, ["kz", "25 kartoshek", "12 manda"],
+rec1 = Recipe("grecha2", 2, 15, ["kz", "25 kartoshek", "12 manda"],
              "nu eta ochen vkusna, pravda", "lunch")
-rec2 = Recipe("grecha", 3, 15, ["kz", "25 kartoshek", "12 manda"],
+rec2 = Recipe("grecha3", 3, 15, ["kz", "25 kartoshek", "12 manda"],
              "nu eta ochen vkusna, pravda", "dessert")
-rec3 = Recipe("grecha", 4, 15, ["kz", "25 kartoshek", "12 manda"],
+rec3 = Recipe("grecha4", 4, 15, ["kz", "25 kartoshek", "12 manda"],
              "nu eta ochen vkusna, pravda", "starter")
 book = Book()
 book.add_recipe(rec)
@@ -16,5 +16,6 @@ book.add_recipe(rec2)
 book.add_recipe(rec3)
 print(book.creation_date.strftime("%c"))
 print(book.last_update.strftime("%c"))
-print(book.get_recipe_by_name("grecha"))
+print(book.get_recipe_by_types("starter"))
+# print(book.get_recipes_by_name("grecha4"))
 
